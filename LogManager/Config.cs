34647af2f239831fa9c8e8468331389e20a24cc7
Fluffball <xeolite@yahoo.com>
2024-03-16 00:52:58 -0400

    Implement basic Remix interface for backup management

--- LogManager/Config.cs
+++ LogManager/Config.cs
@@ -8,9 +8,11 @@ namespace LogManager
 {
     public static class Config
     {
-        public static Configurable<bool> cfgUseAlternativeDirectory;
         public static OptionInterface.ConfigHolder ConfigData => Plugin.OptionInterface.config;
 
+        public static Configurable<bool> cfgUseAlternativeDirectory;
+        public static Configurable<bool> cfgAllowBackups;
+        public static Configurable<bool> cfgAllowProgressiveBackups;
 
         public static void Initialize()
         {
@@ -20,6 +22,16 @@ namespace LogManager
             cfgUseAlternativeDirectory = ConfigData.Bind("cfgUseAlternativeDirectory", false, new ConfigInfo("Choose your Logs folder", new object[]
             {
                 "Prefer StreamingAssets folder for Logs directory"
+            }));
+
+            cfgAllowBackups = ConfigData.Bind("cfgAllowBackups", false, new ConfigInfo("Allow log backups", new object[]
+            {
+                "Backup log files when Rain World starts"
+            }));
+
+            cfgAllowProgressiveBackups = ConfigData.Bind("cfgAllowProgressiveBackups", false, new ConfigInfo("Enable backups for newly detected log files on startup", new object[]
+            {
+                "Automatically enable backups for newly detected log files"
             }));
 
             Plugin.OptionInterface.OnConfigChanged += OptionInterface_OnConfigChanged;
--- LogManager/LoggerOptionInterface.cs
+++ LogManager/LoggerOptionInterface.cs
@@ -10,7 +10,15 @@ namespace LogManager
 {
     internal class LoggerOptionInterface : OptionInterface
     {
+        /// <summary>
+        /// The initial y-value by which all other controls are positioned from 
+        /// </summary>
         private const float y_offset = 560f;
+        
+        /// <summary>
+        /// The position by which controls are aligned along the x-axis
+        /// </summary>
+        private const float x_left_align = 20f;
         public override void Initialize()
         {
             base.Initialize();
@@ -22,24 +30,74 @@ namespace LogManager
 
             OpTab tab = Tabs[0];
 
+            initializePrimaryOptions(tab);
+            initializeBackupOptions(tab);
+        }
+
+        private void initializePrimaryOptions(OpTab tab)
+        {
             //Create elements
-            OpLabel opLabel = new OpLabel(new Vector2(150f, y_offset - 40f), new Vector2(300f, 30f), Translate("Logging Tools"), FLabelAlignment.Center, true, null);
-            OpCheckBox opCheckBox = new OpCheckBox(Config.cfgUseAlternativeDirectory, new Vector2(20f, y_offset - 90f))
+            OpLabel tabHeader = new OpLabel(new Vector2(150f, y_offset - 40f), new Vector2(300f, 30f), Translate("Logging Tools"), FLabelAlignment.Center, true, null);
+            OpCheckBox directoryOptionToggle = new OpCheckBox(Config.cfgUseAlternativeDirectory, new Vector2(x_left_align, y_offset - 90f))
             {
                 description = Translate(Config.cfgUseAlternativeDirectory.info.description)
             };
-            OpLabel infoLabel = new OpLabel(60f, y_offset - 90f, Translate(Config.cfgUseAlternativeDirectory.info.Tags[0] as string), false)
+            OpLabel directoryOptionTooltip = new OpLabel(60f, y_offset - 90f, Translate(Config.cfgUseAlternativeDirectory.info.Tags[0] as string), false)
             {
-                bumpBehav = opCheckBox.bumpBehav,
-                description = opCheckBox.description
+                bumpBehav = directoryOptionToggle.bumpBehav,
+                description = directoryOptionToggle.description
             };
 
             //Add elements to container
             tab.AddItems(new UIelement[]
             {
-                opLabel,
-                opCheckBox,
-                infoLabel
+                tabHeader,
+                directoryOptionToggle,
+                directoryOptionTooltip,
+            });
+        }
+
+        private void initializeBackupOptions(OpTab tab)
+        {
+            float headerOffsetY = y_offset - 150f;
+
+            //Create elements
+            OpLabel backupsHeader = new OpLabel(new Vector2(x_left_align, headerOffsetY), new Vector2(300f, 30f), Translate("Backup Management"), FLabelAlignment.Left, true, null);
+
+            OpCheckBox enableBackupsToggle = new OpCheckBox(Config.cfgAllowBackups, new Vector2(x_left_align, headerOffsetY - 40f))
+            {
+                description = Translate(Config.cfgAllowBackups.info.description)
+            };
+            OpLabel enableBackupsTooltip = new OpLabel(60f, headerOffsetY - 40f, Translate(Config.cfgAllowBackups.info.Tags[0] as string), false)
+            {
+                bumpBehav = enableBackupsToggle.bumpBehav,
+                description = enableBackupsToggle.description
+            };
+
+            OpCheckBox progressiveBackupsToggle = new OpCheckBox(Config.cfgAllowProgressiveBackups, new Vector2(x_left_align, headerOffsetY - 80f))
+            {
+                description = Translate(Config.cfgAllowProgressiveBackups.info.description)
+            };
+            OpLabel progressiveBackupsTooltip = new OpLabel(60f, headerOffsetY - 80f, Translate(Config.cfgAllowProgressiveBackups.info.Tags[0] as string), false)
+            {
+                bumpBehav = enableBackupsToggle.bumpBehav,
+                description = enableBackupsToggle.description
+            };
+
+            OpSimpleButton backupDeleteButton = new OpSimpleButton(new Vector2(x_left_align, headerOffsetY - 140f), new Vector2(120f, 30f), Translate("Delete Backups"))
+            {
+                description = Translate("Removes Backup folder and its contents")
+            };
+
+            //Add elements to container
+            tab.AddItems(new UIelement[]
+            {
+                backupsHeader,
+                enableBackupsToggle,
+                enableBackupsTooltip,
+                progressiveBackupsToggle,
+                progressiveBackupsTooltip,
+                backupDeleteButton
             });
         }
     }
