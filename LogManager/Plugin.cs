85b980003dc0b3faec16be97e838278efef180be
Fluffball <xeolite@yahoo.com>
2026-04-05 22:30:42 -0400

    Update version, and build files

--- LogManager/Plugin.cs
+++ LogManager/Plugin.cs
@@ -15,7 +15,7 @@ namespace LogManager
     {
         public const string PLUGIN_GUID = "fluffball.logmanager";
         public const string PLUGIN_NAME = "Log Manager";
-        public const string PLUGIN_VERSION = "0.9.1";
+        public const string PLUGIN_VERSION = "0.9.3";
 
         private bool hasInitialized;
 
